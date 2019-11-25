21e340f68da29d894dfafb35fd883a52b2e05c36
Heather <a29533342@gmail.com>
2019-11-25 09:54:37 +0800

    first

---
+++ numbergame.py
@@ -0,0 +1,19 @@
+# 產生一個隨機整數1-100(不要印出來)
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出"終於猜對了!"
+# 猜錯的話 要告訴他 比答案大/小
+
+import random
+
+r = random.randint(1, 100)
+print(r)
+while True:
+	ans = input('請輸入一個整數: ')
+	ans = int(ans)
+	if ans < r:
+		print('比答案還要小')
+	elif ans > r:
+		print('比答案還要大')
+	else:
+		print('終於猜對了!')
+		break
